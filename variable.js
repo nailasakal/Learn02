d36c14b83674fc1e6ffd1698ab51645c569bf3b3
Khamdan Nahari <khamdan.nahari@smartfren.com>
2024-02-26 21:18:12 +0700

    Menambahkan beberapa file dasar coading

---
+++ variable.js
@@ -0,0 +1,17 @@
+///kata kunci const(kostanta) a sebuah value yang tidak akan dapat berubah kecuali menggunakan tipe data object dan array
+// const name = "naela";
+// // name = "eduwork";
+// console.log(name);
+
+///Contoh menggunakan tipe data object
+// const student = {
+//   name: "Naela",
+//   class: 2,
+// };
+// student.faculty = "saintexs";
+// console.log(student);
+
+///Contoh menggunakan tipe data array
+// const student = ["Sandi", "Aggung", "Agus"];
+// student.push("eka");
+// console.log(student);
